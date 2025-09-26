23e19c6e7ed0975ce72a3482c53fffbc8cc5f703
Evan-github1 <181235930+Evan-github1@users.noreply.github.com>
2025-09-26 17:18:44 -0400

    ball launcher opmode class for Charles' launcher. speed doesn't adjust correctly

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String APP_BUILD_TIME = "2025-09-25T17:45:43.742-0400";
+  public static final String APP_BUILD_TIME = "2025-09-26T15:57:44.099-0400";
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BallLauncher.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
+
+@TeleOp
+public class BallLauncher extends LinearOpMode {
+
+    private static DcMotor launcherMotorL, launcherMotorR;
+    private static boolean onSwitch = false;
+    private static double speed = .5;
+    private static long time = System.currentTimeMillis();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        launcherMotorL = hardwareMap.get(DcMotor.class, "a");
+        launcherMotorR = hardwareMap.get(DcMotor.class, "b");
+
+        MotorConfigurationType motorConfigurationType = launcherMotorL.getMotorType().clone();
+        motorConfigurationType.setAchieveableMaxRPMFraction(1.0);
+        launcherMotorL.setMotorType(motorConfigurationType);
+        MotorConfigurationType motorConfigurationType2 = launcherMotorR.getMotorType().clone();
+        motorConfigurationType2.setAchieveableMaxRPMFraction(1.0);
+        launcherMotorR.setMotorType(motorConfigurationType2);
+
+        launcherMotorL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        launcherMotorR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        launcherMotorL.setDirection(DcMotorSimple.Direction.FORWARD);
+        launcherMotorR.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            if (gamepad1.y && delay()) {
+                if (onSwitch) {
+                    launcherMotorL.setPower(0);
+                    launcherMotorR.setPower(0);
+                } else {
+                    launcherMotorL.setPower(speed);
+                    launcherMotorR.setPower(speed);
+                }
+                onSwitch = !onSwitch;
+                time = System.currentTimeMillis();
+            } else if (gamepad1.dpad_up && delay()) {
+                speed += .05;
+                time = System.currentTimeMillis();
+            } else if (gamepad1.dpad_down && delay()) {
+                speed -= .05;
+                time = System.currentTimeMillis();
+            }
+
+            telemetry.addData("Speed", speed);
+            telemetry.addData("RPM", launcherMotorR.getPower()); // this doesn't work.
+            telemetry.addData("On?", onSwitch);
+            telemetry.update();
+        }
+
+    }
+
+    private boolean delay() {
+        return System.currentTimeMillis() - time >= 250;
+    }
+
+}
