0f96f1f58dfffd8ef54fbdf817cd2623f467fa40
RockandRook <sengua28@polyprep.org>
2025-10-09 17:46:21 -0400

    Broken. Very broken.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BallLauncher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BallLauncher.java
@@ -13,6 +13,11 @@ public class BallLauncher extends LinearOpMode {
     private static boolean onSwitch;
     private static double speed;
     private static long time;
+    public static final int TICKS_PER_REV = 28;
+    public static int targetRPM = 3000;
+    public static int localStartPos,globalStartPos;
+    public static long localTimeMillis,globalTimeMillis;
+    public static double RPM;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -26,39 +31,59 @@ public class BallLauncher extends LinearOpMode {
         motorConfigurationType2.setAchieveableMaxRPMFraction(1.0);
         launcherMotorR.setMotorType(motorConfigurationType2);
 
-        launcherMotorL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        launcherMotorR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        launcherMotorL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        launcherMotorL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         launcherMotorL.setDirection(DcMotorSimple.Direction.FORWARD);
         launcherMotorR.setDirection(DcMotorSimple.Direction.REVERSE);
 
         onSwitch = false;
-        speed = .5;
+        speed = .1;
         time = System.currentTimeMillis();
 
         waitForStart();
 
+
+
         while (opModeIsActive()) {
             if (gamepad1.y && delay()) {
-                if (onSwitch) {
-                    launcherMotorL.setPower(0);
-                    launcherMotorR.setPower(0);
-                } else {
-                    launcherMotorL.setPower(speed);
-                    launcherMotorR.setPower(speed);
-                }
                 onSwitch = !onSwitch;
                 time = System.currentTimeMillis();
+                if(!onSwitch){
+                    globalStartPos = launcherMotorL.getCurrentPosition();
+                    globalTimeMillis = System.currentTimeMillis();
+                }
             } else if (gamepad1.dpad_up && delay()) {
                 speed += .05;
+                targetRPM += 100;
                 time = System.currentTimeMillis();
             } else if (gamepad1.dpad_down && delay()) {
                 speed -= .05;
+                targetRPM -= 100;
                 time = System.currentTimeMillis();
             }
 
+            try {
+                RPM = (localStartPos / TICKS_PER_REV) * 60000 / localTimeMillis;
+            }catch(Exception ignored){
+
+            }
+
+            if (onSwitch) {
+                launcherMotorL.setPower(speed);
+                launcherMotorR.setPower(speed);
+                localStartPos = launcherMotorL.getCurrentPosition() - globalStartPos;
+                localTimeMillis = System.currentTimeMillis()-globalTimeMillis;
+            } else {
+                launcherMotorL.setPower(0);
+                launcherMotorR.setPower(0);
+
+            }
+
+            telemetry.addData("Local pos",localStartPos);
             telemetry.addData("Speed", speed);
-            telemetry.addData("RPM", launcherMotorR.getPower()); // this doesn't work.
+            telemetry.addData("RPM", launcherMotorR.getPower());
+            telemetry.addData("Actual RPM",RPM);
             telemetry.addData("On?", onSwitch);
             telemetry.update();
         }
